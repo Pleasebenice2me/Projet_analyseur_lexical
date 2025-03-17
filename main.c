101abd944fdd6bf1498a302434ec47f2a2e9d3d7
Pleasebenice2me <ramenabreniere3@gmail.com>
2025-03-17 13:27:45 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,11 +4,11 @@
 
 int main(int argc, char *argv[]){
     int tab;
-    char str();
-    // Ask the user to type a number
-    printf("Type your function: \n");
-    // Get and save the number the user types
-    scanf("%c", &str);
-    tab = analyselexical(char str);
+    char str;
+    // Ask the user to type a character
+    printf("Type your character: \n");
+    // Get and save the character the user types
+    scanf(" %c", &str); // Added a space before %c to skip any whitespace
+    tab = analyselexical(str); // Removed char from the function call
     return 0;
 }
