d17ba5c428db7aee6876b11879282f60cae474c3
Pleasebenice2me <ramenabreniere3@gmail.com>
2025-03-17 13:23:49 +0100

    Update analyse_lex.c

--- analyse_lex.c
+++ analyse_lex.c
@@ -1,43 +1,41 @@
-
-
-int analyselexical(char str()){
+int analyselexical(char* str){
     int i = 0;
     while(str[i] != "\0"){
         switch (str[i]){
-            case "+":
+            case '+':
                 T[i].lexem = OPERATEUR;
                 T[i].valeur.operateur = PLUS;
                 break;
-            case "-":
+            case '-':
                 T[i].lexem = OPERATEUR;
                 T[i].valeur.operateur = MOINS;
                 break;
-            case "*":
+            case '*':
                 T[i].lexem = OPERATEUR;
                 T[i].valeur.operateur = FOIS;
                 break;
-            case "/":
+            case '/':
                 T[i].lexem = OPERATEUR;
                 T[i].valeur.operateur = DIV;
                 break;
-            case "^":
+            case '^':
                 T[i].lexem = OPERATEUR;
                 T[i].valeur.operateur = PUIS;
                 break;
-            case "(":
+            case '(':
                 T[i].lexem = PAR_OUV;
                 break;
-            case ")":
+            case ')':
                 T[i].lexem = PAR_FERM;
                 break;
-            case "[":
+            case '[':
                 T[i].lexem = BAR_OUV;
                 break;
-            case "]":
+            case ']':
                 T[i].lexem = BAR_FERM;
                 break;
             default : {
-                if (str[i]=> "a" & str[i]=< "z"){
+                if (str[i] >= 'a' && str[i] <= 'z'){
                     //faire une chaine de caractere provisoir
 
                     T[i].lexem = FONCTION;
